16d4bd28dca1440917d6c92b1708a36e89ea7e81
MarcIlio19 <77625568+MarcIlio19@users.noreply.github.com>
2021-01-19 11:29:54 -0300

    Update URI_2012_Área.cs

--- aula 4/URI_2012_Área.cs
+++ aula 4/URI_2012_Área.cs
@@ -1,4 +1,15 @@
+using System;
+class MainClass {
+  public static void Main (string[] args){
+string[] valores = Console.ReadLine.split('');
+double A = int.parse(valores[0]);
+double B = int.Parse(valores[1]);
+double c = int.Parse(valores[2]);
 
+double triangulo = (A*C)/2;
+Console.WriteLine(triangulo);
+ }
+}
 
 
 
