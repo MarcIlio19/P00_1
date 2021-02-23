45a13f9abcea84c40c8e80433b853ff13c3bd627
MarcIlio19 <backen124@gmail.com>
2021-02-23 19:43:36 +0000

    lista 9, exercicio 1

--- lista_9/1.cs
+++ lista_9/1.cs
@@ -1,1 +1,25 @@
 
+using System;
+class MainClass{
+  public static void Main(string[] args){
+  calcircunferencia y = new calcircunferencia();
+  y.setraio(double.Parse(Console.ReadLine()));
+  Console.WriteLine(y.circunferencia());
+  }
+}
+class calcircunferencia{
+  private double raio;
+  public void setraio(double v ){
+  if(v > 0)
+    raio = v;
+  }
+  public double getraio(){
+  return raio;
+  }
+
+  public double circunferencia(){
+   double circunferencia = raio * 2;
+    return  circunferencia;
+  }
+ 
+}
